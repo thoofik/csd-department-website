a0a5bd00bfe1bc31d012a41fbe8fd61cd6254055
thoofik <thoofikusmaan@gmail.com>
2025-09-10 01:41:22 +0530

    Add PWA splash screens with white background
    - Generated splash screen images for all major mobile devices
    - Created splash screens with white background and centered light logo
    - Added Apple-specific splash screen meta tags to layout.tsx
    - Updated manifest.json with splash screen configuration
    - Fixed mobile app loading screen to show white background instead of black
    - Covers iPhone 5/6/7/8/X/11/12/13/14/15 and Android devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -71,6 +71,19 @@ export default function RootLayout({
             `,
           }}
         />
+        {/* Apple Splash Screen Meta Tags */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PESITM CSD" />
+        
+        {/* iPhone Splash Screens */}
+        <link rel="apple-touch-startup-image" href="/splash-640x1136.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash-750x1334.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash-828x1792.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" />
+        <link rel="apple-touch-startup-image" href="/splash-1125x2436.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" />
+        <link rel="apple-touch-startup-image" href="/splash-1242x2688.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" />
+        <link rel="apple-touch-startup-image" href="/splash-1284x2778.png" media="(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3)" />
+        <link rel="apple-touch-startup-image" href="/splash-1170x2532.png" media="(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3)" />
       </head>
       <body className={`${inter.className} min-h-screen overflow-x-hidden`}>
         <ThemeProvider>
